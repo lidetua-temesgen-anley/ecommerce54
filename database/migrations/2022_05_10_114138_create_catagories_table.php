cc68eaec5ce8e6a6bf81567c52a99754fe4de730
lidetua-temesgen-anley <lidetuatemesgen123@gmail>
2022-05-14 18:05:31 +0100

    commit

--- database/migrations/2022_05_10_114138_create_catagories_table.php
+++ database/migrations/2022_05_10_114138_create_catagories_table.php
@@ -4,32 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateCatagoriesTable extends Migration
 {
-    /*
+    /**
      * Run the migrations.
      *
      * @return void
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('catagory', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
-            $table->string('unit')->nullable();
-            $table->double('price',82)->nullable();
-            $table->integer('quantity')->nullable();
+            $table->timestamps();
         });
     }
 
-    /*
+    /**
      * Reverse the migrations.
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('catagories');
     }
 }
